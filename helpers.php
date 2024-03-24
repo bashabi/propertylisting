f2b1572b885b277a230207ebf42a94cd131b6910
bashabi <bashabi@themonsoon.com>
2024-03-24 20:45:04 +0000

    creating partial

--- helpers.php
+++ helpers.php
@@ -11,3 +11,29 @@ function basePath($path = '')
 {
     return __DIR__ . '/' . $path;  // get the absoulute path
 }
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name)
+{
+
+    require basePath("views/{$name}.view.php");
+}
+
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadPartial($name)
+{
+    require basePath("views/partials/{$name}.php");
+}
---
+++ views/partials/top-banner.php
@@ -0,0 +1,9 @@
+<!-- Top Banner -->
+<section class="bg-blue-900 text-white py-6 text-center">
+    <div class="container mx-auto">
+        <h2 class="text-3xl font-semibold">Explore Your Property Potential</h2>
+        <p class="text-lg mt-2">
+            Discover the perfect home for you.
+        </p>
+    </div>
+</section>
